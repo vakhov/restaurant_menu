d225bcb761513e46e8e6904c4773427b7f367aea
Alex Vakhov <alex.vakhov@gmail.com>
2020-10-04 20:35:44 +0500

    [add] base menu models

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_display_links = list_display
+    search_fields = ('id', 'title',)
+    list_filter = ('title',)
+
+
+@admin.register(Allergen)
+class AdminAllergen(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_display_links = list_display
+    search_fields = ('id', 'title',)
+    list_filter = ('title',)
+
+
+@admin.register(Menu)
+class AdminMenu(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'price',)
+    list_display_links = ('id', 'title', 'category',)
+    list_editable = ('price',)
+    list_filter = ('title', 'category', 'price',)
+    raw_id_fields = ('category',)
+    filter_horizontal = ('allergens',)
--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,60 @@
+from django.core.validators import DecimalValidator
 from django.db import models
+from easy_thumbnails.fields import ThumbnailerImageField
 
-# Create your models here.
+from utils.helpers import generate_upload_name
+
+__all__ = ['Category', 'Allergen', 'Menu']
+
+
+class Category(models.Model):
+    """
+    Категории
+    """
+    title = models.CharField('Наименование', max_length=50, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        unique_together = ('title',)
+        ordering = ('title',)
+
+
+class Allergen(models.Model):
+    """
+    Аллергены
+    """
+    title = models.CharField('Наименование', max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Аллерген'
+        verbose_name_plural = 'Аллергены'
+        unique_together = ('title',)
+        ordering = ('title',)
+
+
+class Menu(models.Model):
+    """
+    Меню
+    """
+    category = models.ForeignKey(Category, models.SET_NULL, 'menu_items', verbose_name='Категория', null=True)
+    title = models.CharField('Название', max_length=100, db_index=True)
+    price = models.DecimalField('Стоимость', max_digits=5, decimal_places=2, default=350,
+                                validators=[DecimalValidator(5, 2)])
+    image = ThumbnailerImageField('Изображение', upload_to=generate_upload_name, null=True, blank=True)
+    allergens = models.ManyToManyField(Allergen, verbose_name='Аллергены')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
+        unique_together = ('title',)
+        ordering = ('category', 'title',)
