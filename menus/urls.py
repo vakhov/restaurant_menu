5db211c038bcfa68b446ae406c9b5ca43eba7f41
Alex Vakhov <alex.vakhov@gmail.com>
2020-10-04 22:56:30 +0500

    [add] order

--- menus/urls.py
+++ menus/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CategoryListView
+from .views import CategoryListView, OrderTemplateView
 
 app_name = 'menus'
 
 urlpatterns = [
     path('', CategoryListView.as_view(), name='category-list'),
+    path('order/', OrderTemplateView.as_view(), name='order-view'),
 ]
--- menus/views.py
+++ menus/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.db.models import Sum
+from django.views.generic import ListView, TemplateView
 
-from menus.models import Category
+from menus.models import *
 
 
 class CategoryListView(ListView):
     """Список категорий и их значений (меню)"""
     model = Category
     queryset = Category.objects.prefetch_related('menu_items')
+
+
+class OrderTemplateView(TemplateView):
+    template_name = 'menus/order.html'
+
+    def post(self, request, **kwargs):
+        menu_ids = dict(request.POST).get('menu', [])
+        menu_items = Menu.objects.filter(id__in=menu_ids)
+        allergens = Allergen.objects.filter(menu__in=menu_items).distinct()
+        context = {
+            'menu_items': menu_items,
+            'allergens': allergens,
+            'amount': menu_items.aggregate(Sum('price'))['price__sum']
+        }
+
+        return super().render_to_response(context)
