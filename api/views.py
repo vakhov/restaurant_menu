d5e8de473c921264a023f4160d87af2f93eaec9d
Alex Vakhov <alex.vakhov@gmail.com>
2020-10-05 07:58:57 +0500

    [refactor] protection through the key specified in the settings

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from rest_framework import mixins
+from rest_framework.exceptions import NotAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
 from api.serializers import RestaurantMenuSerializer
@@ -10,3 +12,9 @@ class RestaurantMenuListCreateAPIView(mixins.CreateModelMixin,
                                       GenericViewSet):
     queryset = Menu.objects.filter(category__isnull=False)
     serializer_class = RestaurantMenuSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        token = request.POST.get('token', None) or request.GET.get('token', None)
+        if not token or token != settings.TOKEN_KEY:
+            raise NotAuthenticated()
+        return super().dispatch(request, *args, **kwargs)
