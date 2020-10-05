c7c45185a51fdb109a69ef4e6f1fb81495175b7c
Alex Vakhov <alex.vakhov@gmail.com>
2020-10-05 11:31:46 +0500

    [refactor] permission classes Class

--- api/views.py
+++ api/views.py
@@ -1,20 +1,25 @@
 from django.conf import settings
 from rest_framework import mixins
-from rest_framework.exceptions import NotAuthenticated
+from rest_framework.permissions import BasePermission
 from rest_framework.viewsets import GenericViewSet
 
 from api.serializers import RestaurantMenuSerializer
 from menus.models import Menu
 
 
+class TokenRequired(BasePermission):
+
+    def has_permission(self, request, view):
+        token = request.data.get('token') or request.POST.get('token') or request.GET.get('token')
+        if not token or token != settings.TOKEN_KEY:
+            return False
+        return True
+
+
 class RestaurantMenuListCreateAPIView(mixins.CreateModelMixin,
                                       mixins.ListModelMixin,
                                       GenericViewSet):
+    """Добавление и просмотр меню"""
     queryset = Menu.objects.filter(category__isnull=False)
     serializer_class = RestaurantMenuSerializer
-
-    def dispatch(self, request, *args, **kwargs):
-        token = request.POST.get('token', None) or request.GET.get('token', None)
-        if not token or token != settings.TOKEN_KEY:
-            raise NotAuthenticated()
-        return super().dispatch(request, *args, **kwargs)
+    permission_classes = (TokenRequired,)
